4104e7f50441169198d671430918383d9bcde777
Kevin <kevinyahirt@gmail.com>
2024-11-19 21:19:36 -0600

    Estados definidos para el automata del correo

--- Automatas_ep3/Automatas_ep3/src/main/java/com/mycompany/automatas_ep3/AutomataEmail.java
+++ Automatas_ep3/Automatas_ep3/src/main/java/com/mycompany/automatas_ep3/AutomataEmail.java
@@ -6,5 +6,50 @@ import lombok.Setter;
 public class AutomataEmail {
     
     @Setter private String cinta;
+    
+    public boolean estadoA(int posicion){
+        if(posicion == cinta.length()) return false;
+        return false;
+    } 
+    
+    public boolean estadoB(int posicion){
+        return false;
+    }  
 
+    public boolean estadoC(int posicion){
+        return false;
+    }
+    
+    public boolean estadoD(int posicion){
+        return false;
+    }
+    
+    public boolean estadoE(int posicion){
+        return false;
+    }
+    
+    public boolean estadoF(int posicion){
+        return false;
+    }
+    
+    public boolean estadoG(int posicion){
+        return false;
+    }
+    
+    public boolean estadoH(int posicion){
+        return false;
+    }
+    
+    public boolean estadoI(int posicion){
+        return false;
+    }
+    
+    public boolean estadoJ(int posicion){
+        return false;
+    }
+    
+    public boolean estadoK(int posicion){
+        return false;
+    }
+    
 }
